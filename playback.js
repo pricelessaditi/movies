00fa0cc9df7766a237ce335429e907c82f33fd53
Chris George <74033835+pricelessaditi@users.noreply.github.com>
2023-11-19 11:42:04 +0530

    Update playback.js

--- playback.js
+++ playback.js
@@ -1,12 +1,31 @@
-var wrapper = Popcorn.HTMLYouTubeVideoElement("#video");
-wrapper.src = "https://www.youtube.com/shorts/IETnIoz-jFQ";
-pop = Popcorn(wrapper);
+(function( window, Popcorn ) {
 
-pop.footnote({
-  start: 1,
-  end: 5,
-  text: "Works with youtube!",
-  target: "footnote-div"
-});
+  var canPlayType = function( nodeName, url ) {
+    return ( typeof url === "string" &&
+             Popcorn.HTMLYouTubeVideoElement._canPlaySrc( url ) );
+  };
 
-pop.play();
+  Popcorn.player( "youtube", {
+    _canPlayType: canPlayType
+  });
+
+  Popcorn.youtube = function( container, url, options ) {
+    if ( typeof console !== "undefined" && console.warn ) {
+      console.warn( "Deprecated player 'youtube'. Please use Popcorn.HTMLYouTubeVideoElement directly." );
+    }
+
+    var media = Popcorn.HTMLYouTubeVideoElement( container ),
+        popcorn = Popcorn( media, options );
+
+    // Set the src "soon" but return popcorn instance first, so
+    // the caller can listen for error events.
+    setTimeout( function() {
+      media.src = url;
+    }, 0 );
+
+    return popcorn;
+  };
+
+  Popcorn.youtube.canPlayType = canPlayType;
+
+}( window, Popcorn ));
