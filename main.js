12f3ea6f4ba18a9d80050d6ecccf2f8c7d853bca
Chris George <74033835+pricelessaditi@users.noreply.github.com>
2023-11-19 12:08:39 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-const video1URL = 'https://drive.google.com/your_video1_url';
-const video2URL = 'https://drive.google.com/your_video2_url';
+const video1URL = 'videos/video1.mp4';
+const video2URL = 'videos/video2.mp4';
 
 document.addEventListener('DOMContentLoaded', function () {
-    // Initialize Popcorn instances
     const popcorn1 = Popcorn('#video1');
     const popcorn2 = Popcorn('#video2');
 
-    // Set up synchronized playback
     popcorn1.autoplay(true);
     popcorn2.autoplay(true);
 
@@ -18,7 +16,6 @@ document.addEventListener('DOMContentLoaded', function () {
         popcorn1.currentTime(popcorn2.currentTime());
     });
 
-    // Load Google Drive videos
     popcorn1.src(video1URL, 'video/mp4');
     popcorn2.src(video2URL, 'video/mp4');
 });
